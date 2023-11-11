4ae3e60b0655b532a16b156cb58a1bcda92e4685
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-11-11 23:31:36 +0200

    Add base class with SetUp and TearDown

--- SeleniumTests/BaseTests/BaseTest.cs
+++ SeleniumTests/BaseTests/BaseTest.cs
@@ -6,5 +6,16 @@ namespace SeleniumTests.BaseTests
 {
     internal class BaseTest
     {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.InitializeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.QuitDriver();
+        }
     }
 }
