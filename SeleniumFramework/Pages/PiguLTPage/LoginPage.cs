2311303149b2ba4bba024082c96345925871405a
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-11-11 23:30:43 +0200

    Add login page class functionality

--- SeleniumFramework/Pages/PiguLTPage/LoginPage.cs
+++ SeleniumFramework/Pages/PiguLTPage/LoginPage.cs
@@ -2,6 +2,56 @@
 {
     public class LoginPage
     {
+        public static void Open()
+        {
+            Driver.OpenPage("https://pigu.lt/lt/");
+        }
 
+        public static void ClickOnProfileIcon()
+        {
+            string locator = Locators.LoginPage.IconProfile;
+            Common.ClickElement(locator);
+        }
+
+        public static void ClickOnBlueLoginButton()
+        {
+            string locator = Locators.LoginPage.BlueLoginButton;
+            Common.ClickElement(locator);
+        }
+
+        public static void EnterLoginEmail(string loginEmail)
+        {
+            string locator = Locators.LoginPage.InputEmail;
+            Common.SendKeysToElement(locator, loginEmail);
+        }
+
+        public static void EnterLoginPassword(string loginPassword)
+        {
+            string locator = Locators.LoginPage.InputPassword;
+            Common.SendKeysToElement(locator, loginPassword);
+        }
+
+        public static void ClickOnGreenLoginButton()
+        {
+            string locator = Locators.LoginPage.GreenLoginButton;
+            Common.ClickElement(locator);
+        }
+
+        public static void ClickOnMyAccount()
+        {
+            string locator = Locators.LoginPage.MyAccount;
+            Common.ClickElement(locator);
+        }
+
+        public static void ClickOnEditProfile()
+        {
+            string locator = Locators.LoginPage.EditProfile;
+            Common.ClickElement(locator);
+        }
+
+        public static string GetMyAccountName()
+        {
+            return Common.GetElementText(Locators.LoginPage.MyAccountName);
+        }
     }
 }
