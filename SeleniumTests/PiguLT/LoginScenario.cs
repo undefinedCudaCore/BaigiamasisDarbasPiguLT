48e8681f14c0d2b710666e48c1e35cf84a5aca75
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-11-11 23:34:02 +0200

    Add step logic for LoginPage

--- SeleniumTests/PiguLT/LoginScenario.cs
+++ SeleniumTests/PiguLT/LoginScenario.cs
@@ -7,6 +7,22 @@ namespace SeleniumTests.PiguLT
 {
     internal class LoginScenario : BaseTest
     {
+        [Test]
+        public void LoginToWebShopAccount()
+        {
+            string loginEmail = "testforgraduationthesis@gmail.com";
+            string loginPassword = "seleniumtestforgraduationthesis";
 
+            LoginPage.Open();
+            LoginPage.ClickOnProfileIcon();
+            LoginPage.ClickOnBlueLoginButton();
+            LoginPage.EnterLoginEmail(loginEmail);
+            LoginPage.EnterLoginPassword(loginPassword);
+            LoginPage.ClickOnGreenLoginButton();
+            LoginPage.ClickOnProfileIcon();
+            LoginPage.ClickOnMyAccount();
+            LoginPage.ClickOnEditProfile();
+            LoginPage.GetMyAccountName();
+        }
     }
 }
