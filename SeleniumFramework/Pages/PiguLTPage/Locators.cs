d1252eeede340479fcc7742f2f3483481198ea38
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-11-11 23:45:56 +0200

    Add update for locators

--- SeleniumFramework/Pages/PiguLTPage/Locators.cs
+++ SeleniumFramework/Pages/PiguLTPage/Locators.cs
@@ -5,10 +5,10 @@
         internal class LoginPage
         {
             internal static string IconProfile = "//*[@id='headeMenu']/li[1]/a/i";
-            internal static string BlueLoginButton = "//*[@id='headeMenu']/li[1]/div/div/div[2]/a[1]";
-            internal static string InputEmail = "//*[@id='loginModal']/div[1]/div[1]/form/div[3]/input";
-            internal static string InputPassword = "//*[@id='passwordCont']/input";
-            internal static string GreenLoginButton = "//*[@id='loginModal']/div[1]/div[1]/form/div[6]/input";
+            internal static string BlueLoginButton = "//a[contains(@href, 'https://pigu.lt/lt/u/login')]";
+            internal static string InputEmail = "//input[@name='email']";
+            internal static string InputPassword = "//input[@name='password']";
+            internal static string GreenLoginButton = "//input[@name='login']";
             internal static string MyAccount = "//*[@id='headeMenu']/li[1]/div/ul[1]/li/a";
             internal static string EditProfile = "//a[contains(@href, 'u/info')]";
             internal static string MyAccountName = "//*[@id='editMyInfo']/div[2]/div/div[1]";
