9307f94a19c9377279c1a30015fca82ade1fadb9
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-11-12 19:06:51 +0200

    Add scenario test steps and assert

--- SeleniumTests/PiguLT/SearchItemScenario.cs
+++ SeleniumTests/PiguLT/SearchItemScenario.cs
@@ -7,5 +7,33 @@ namespace SeleniumTests.PiguLT
 {
     internal class SearchItemScenario : BaseTest
     {
+        [Test]
+        public void SearchItemInWebShop()
+        {
+            string itemSearchPhrase = "X515EA-BQ1735W";
+            string foundProductName = "Nešiojamas kompiuteris Asus X515EA-BQ1735W";
+            string manufacturerCode = "X515EA-BQ1735W";
+
+            SearchItemPage.Open();
+            //SearchItemPage.ClickAllowAllCookies();
+            //SearchItemPage.Waiting();
+            SearchItemPage.EnterSearchPhrase(itemSearchPhrase);
+            SearchItemPage.ClickSearchButton();
+            SearchItemPage.ClickFoundItemImageButton();
+            //SearchItemPage.ScrollIntoView();
+            //SearchItemPage.ClickMoreInformationButton();
+
+            try
+            {
+                Assert.That(SearchItemPage.GetProductName(), Is.EqualTo(foundProductName));
+                Assert.That(SearchItemPage.ProductManufacturerCode(), Is.EqualTo(manufacturerCode));
+            }
+            catch (Exception ex)
+            {
+                SearchItemPage.GetTestFailScreenshot();
+
+                throw;
+            }
+        }
     }
 }
