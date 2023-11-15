006165cfc289b90513ff5c0d09f17d4a1297c475
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-11-15 16:46:45 +0200

    WishlistPage methods and AddItemToWishlist scenario logic

--- SeleniumTests/PiguLT/AddItemToWishlist.cs
+++ SeleniumTests/PiguLT/AddItemToWishlist.cs
@@ -6,6 +6,20 @@ namespace SeleniumTests.PiguLT
 {
     internal class AddItemToWishlist : BaseTest
     {
+        [Test]
+        public void AddItemToWishList()
+        {
+            string itemSearchPhrase = "pixel 8 pro";
 
+            LoginPage.ClickOnProfileIcon();
+            LoginPage.ClickOnBlueLoginButton();
+            LoginPage.EnterLoginEmail(Strings.LoginString.InputEmail1);
+            LoginPage.EnterLoginPassword(Strings.LoginString.InputPassword);
+            LoginPage.ClickOnGreenLoginButton();
+            //WishlistPage.ClickAllowAllCookies();
+            WishlistPage.EnterSearchPhrase(itemSearchPhrase);
+            WishlistPage.ClickSearchButton();
+            WishlistPage.ClickAddToWishlistButton();
+        }
     }
 }
