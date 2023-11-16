b61aa74ceb686bda5ed594db44f1ed7b71b47308
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-11-16 19:36:18 +0200

    Add Order page methods

--- SeleniumFramework/Pages/PiguLT/OrderPage.cs
+++ SeleniumFramework/Pages/PiguLT/OrderPage.cs
@@ -4,5 +4,33 @@ namespace SeleniumFramework.Pages.PiguLT
 {
     public class OrderPage
     {
+        public static void ClickInformationButton()
+        {
+            string locator = Locators.OrderPage.Information;
+            Common.ClickElement(locator);
+        }
+
+        public static void ClickTrackOrderButton()
+        {
+            string locator = Locators.OrderPage.TrackOrder;
+            Common.ClickElement(locator);
+        }
+
+        public static void ClickSelectOrderDropdown()
+        {
+            string locator = Locators.OrderPage.SelectOrderDropdown;
+            Common.ClickElement(locator);
+        }
+
+        public static void ClickSelectOrderDropdownValue()
+        {
+            string locator = Locators.OrderPage.SelectOrderDropdownValue;
+            Common.ClickElement(locator);
+        }
+
+        public static string GetOrderStatus()
+        {
+            return Common.GetElementText(Locators.OrderPage.OrderInformation);
+        }
     }
 }
